fde5c5b5df3a2004ef4b456be7379bb3212b0628
datboiluiskrrt <datboiluiskrrt@gmail.com>
2022-02-02 14:25:20 -0600

    Styling the register component

--- frontend/anywhere-fitness-08/src/components/Register.js
+++ frontend/anywhere-fitness-08/src/components/Register.js
@@ -2,126 +2,103 @@ import React, { useState } from "react";
 import styled from "styled-components";
 
 const StyledRegister = styled.div`
-    border: black solid 1px;
+  border: black solid 1px;
+  width: 50%;
+  padding: 100px 0;
+  padding-top: 20px;
+
+  padding: 20px 0 40px 0px;
+  margin-top: 10vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  button {
+    margin-top: 2rem;
+  }
+
+  form {
     width: 50%;
-    padding: 100px 0;
-    padding-top: 20px;
-
-    padding: 20px 0 40px 0px;
-    margin-top: 10vh;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    
-
-    button{
-        margin-top: 2rem;
-    }
-    
-    form{
-        width: 50%;
-    }
-`
-
+  }
+`;
 
 const Register = () => {
+  const [form, setForm] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    username: "",
+    password: "",
+    ofAge: false,
+    accountType: "",
+  });
 
-    const [ form, setForm ] = useState({
-        firstName: '',
-        lastName: '',
-        email: '',
-        username: '',
-        password: '',
-        ofAge: false,
-        accountType: ''
-    });
+  const handleChange = (e) => {
+    setForm({ ...form, [e.target.name]: e.target.value });
+  };
 
-    const handleChange = e => {
-        setForm({...form,
-        [e.target.name]: e.target.value})
-    }
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
 
-    const handleSubmit = e => {
-        e.preventDefault();
-    }
+  return (
+    <div className="register-container">
+      <h1>
+        Register your new account below to get started on your workout journey
+      </h1>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>
+            <span>First Name:</span>
+            <input type="input" name="firstName" onChange={handleChange} />
+          </label>
+        </div>
 
-    return(
-        <StyledRegister>
-            <h1>Register your new account below to get started on your workout journey</h1>
-            <form onSubmit={handleSubmit}>
-                <div>
-                    <label><span>First Name:</span>
-                        <input 
-                            type='input'
-                            name='firstName'
-                            onChange={handleChange}
-                        />
-                    </label>
-                </div>
+        <div>
+          <label>
+            Last Name:
+            <input type="input" name="lastName" onChange={handleChange} />
+          </label>
+        </div>
 
-                <div>
-                    <label>Last Name:
-                        <input 
-                            type='input'
-                            name='lastName'
-                            onChange={handleChange}
-                        />
-                    </label>
-                </div>
-                
-                <div>
-                    <label>Email:
-                        <input 
-                            type='input'
-                            name='email'
-                            onChange={handleChange}
-                        />
-                    </label>
-                </div>
+        <div>
+          <label>
+            Email:
+            <input type="input" name="email" onChange={handleChange} />
+          </label>
+        </div>
 
-                <div>
-                    <label>Username: 
-                        <input 
-                            type='input'
-                            name='username'
-                            onChange={handleChange}
-                        />
-                    </label>
-                </div>
+        <div>
+          <label>
+            Username:
+            <input type="input" name="username" onChange={handleChange} />
+          </label>
+        </div>
 
-                <div>
-                    <label>Password: 
-                        <input 
-                            type='password'
-                            name='password'
-                            onChange={handleChange}
-                        />
-                    </label>
-                </div>
+        <div>
+          <label>
+            Password:
+            <input type="password" name="password" onChange={handleChange} />
+          </label>
+        </div>
 
-                <div>
-                    <label> 
-                        <input 
-                            type='checkbox'
-                            onChange={handleChange}
-                        />
-                        Are you at least 18 years old?
-                    </label>
-                </div>
-                
-                <div>
-                    <label> 
-                        <input 
-                            type='checkbox'
-                            onChange={handleChange}
-                        />
-                        Do you agree with our terms of service?
-                    </label>
-                </div>
-                <button>Submit</button>
-            </form>
-        </StyledRegister>
-    )
-}
+        <div>
+          <label>
+            <input type="checkbox" onChange={handleChange} />
+            Are you at least 18 years old?
+          </label>
+        </div>
+
+        <div>
+          <label>
+            <input type="checkbox" onChange={handleChange} />
+            Do you agree with our terms of service?
+          </label>
+        </div>
+        <button className="register-button">Submit</button>
+      </form>
+    </div>
+  );
+};
 
 export default Register;
